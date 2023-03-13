c3d248d17e1535a1e71d8df3270fc3bc6eefff59
IvanTeok <127732750+IvanTeok@users.noreply.github.com>
2023-03-13 17:12:59 +0800

    FYP

---
+++ Customer Page/mainpage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Main Page</title>
+        <style>
+        </style>
+    </head>
+    <body>
+
+        <?php
+            include('header.php');
+            include('swiper.php');
+            include('footer.php');
+        ?>
+
+</html>
