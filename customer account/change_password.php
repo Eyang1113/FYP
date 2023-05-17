373007a337943c0326eb20342e0cad010691e75c
LimChingHong <limchinghong5355@gmail.com>
2023-05-18 03:29:15 +0800

    udpated

--- customer account/change_password.php
+++ customer account/change_password.php
@@ -65,6 +65,8 @@ else
 				name="confirm-new-password"
 				id="confirm-new-password" 
 				placeholder="Confirm New Password"
+				pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
 				required>
 				<br>
 
